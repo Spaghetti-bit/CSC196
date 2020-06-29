bb306e95886b51cdee7cd41c798ae51eca9389bf
Spaghetti-bit <yumyumkillkill@gmail.com>
2020-06-29 11:04:56 -0700

    Assignment - Project Setup. - Update - Draw - Main

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,12 +1,31 @@
 // Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include "core.h"
 #include <iostream>
+
+bool Update(float dt)
+{
+	return false;
+}
+
+void Draw(Core::Graphics& graphics)
+{
+	graphics.SetColor(RGB(rand() % 256, rand() % 256, rand() % 256));
+	graphics.DrawLine(static_cast<float>(rand() % 800), static_cast<float>(rand() % 600), static_cast<float>(rand() % 800), static_cast<float>(rand() % 600));
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	char name[] = "CSC196";
+	Core::Init(name, 800, 600);
+	Core::RegisterUpdateFn(Update);
+	Core::RegisterDrawFn(Draw);
+
+	Core::GameLoop();
+	Core::Shutdown();
 }
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
